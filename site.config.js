39ed084c2abf7f087b83f1ab07e631b1e5eb62a4
Ryu Hyunwoo <wowo0118@sasa.hs.kr>
2023-11-22 14:31:25 +0900

    Update

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Hyunwoo Ryu",
-    image: "/profile-circle.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/profile.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Bioinformatics developer",
     bio: "Biological science & Chemistry & Computer science and so on!",
     email: "ryu@hyunwoo.info",
